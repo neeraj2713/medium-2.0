bf54e3085a9ba5f11629b3650cecd280a6e36713
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-07-18 09:41:05 +0530

    added all endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
 import { sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
   Bindings: {
@@ -10,77 +10,11 @@ const app = new Hono<{
   }
 }>()
 
-app.use('/api/v1/blog/*', async (c, next) => {
-  const header = c.req.header("authorization") || ""
-  const token = header.split(" ")[1]
-
-  const response = await verify(token, c.env.JWT_SECRET)
-  if(response.id){
-    next()
-  } else {
-    c.status(403)
-    return c.json({error : "unauthorized"})
-  }
-})
-
-app.post('/api/v1/signup', async (c) => {
-
-	const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-
-  const body = await c.req.json();
-
-  const user = await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password
-    }
-  })
-
-  const token = await sign({id: user.id}, c.env.JWT_SECRET)
-  
-  return c.json({
-    jwt: token
-  })
-})
-
-app.post('/api/v1/signin', async (c) => {
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
-	const user = await prisma.user.findUnique({
-		where: {
-			email: body.email,
-      password: body.password
-		}
-	});
-
-	if (!user) {
-		c.status(403);
-		return c.json({ error: "user not found" });
-	}
-
-	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-	return c.json({ jwt });
-})
+app.route("/api/v1/user", userRouter)
+app.route("api/v1/blog", blogRouter)
 
 
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
 
-app.post('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
-
-app.put('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
 
 
 export default app
